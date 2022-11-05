c7e006c30f39d3cc1de9dffbaad4957ee9af8f24
Uzhastin-Nikita <fosfor20@mail.ru>
2022-11-05 22:33:28 +0300

    feat(server): add routes for config api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 
+import configRoutes from './api/configRoutes';
+
 export const runServer = async () => {
     const app = express();
-    
+    app.use('/', configRoutes);
     app.use(bodyParser.json());
-    
-    app.get('/api', (req, res) => {
-        res.send('Hello World!');
-    });
+
     
     app.listen(3000, () => {
         console.log('Server running on port 3000');
